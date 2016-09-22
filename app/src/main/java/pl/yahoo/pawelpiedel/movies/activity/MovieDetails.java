4cc1eadc85600f0fa18bc63eb48f38f4237d0ca6
PPiedel <pawelpiedel@yahoo.pl>
2016-09-22 13:40:45 +0200

    MovieDetails refactored to use ButterKnife. Release date icon added.

--- app/src/main/java/pl/yahoo/pawelpiedel/movies/activity/MovieDetails.java
+++ app/src/main/java/pl/yahoo/pawelpiedel/movies/activity/MovieDetails.java
@@ -2,13 +2,11 @@ package pl.yahoo.pawelpiedel.movies.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -19,6 +17,8 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import pl.yahoo.pawelpiedel.movies.R;
 import pl.yahoo.pawelpiedel.movies.adapter.MoviesAdapter;
 import pl.yahoo.pawelpiedel.movies.model.Movie;
@@ -27,24 +27,29 @@ import pl.yahoo.pawelpiedel.movies.utils.ScrollUtils;
 
 public class MovieDetails extends AppCompatActivity{
     private Movie movie;
-    private ScrollView mScrollView;
-    private ActionMenuView amvMenu;
-    private View imgContainer;
-    private TextView reviewView;
-    private ImageView movieImageView;
-    private View mToolbarView;
-    int mParallaxImageHeight;
+    private int mParallaxImageHeight;
+
+    @BindView(R.id.scroll) ScrollView mScrollView;
+    @BindView(R.id.movie_detail_toolbar) Toolbar toolbar;
+    @BindView(R.id.amvDetailsMenu) ActionMenuView amvMenu;
+    @BindView(R.id.review) TextView reviewView;
+    @BindView(R.id.movie_details_image) ImageView movieImageView;
+    @BindView(R.id.ratingBar) RatingBar ratingBar;
+    @BindView(R.id.details_movie_title) TextView movieTitle;
+    @BindView(R.id.release_date) TextView releaseDateTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = setToolbar();
+        setToolbar();
 
-        movie = getMovieFromIntent();
+        movie = getMovieFromIntentExtras();
 
-        mScrollView = (ScrollView) findViewById(R.id.scroll);
         mScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
@@ -52,48 +57,46 @@ public class MovieDetails extends AppCompatActivity{
 
                 int baseColor = getResources().getColor(R.color.colorPrimary);
                 float alpha = Math.min(1, (float) scrollY / mParallaxImageHeight);
-                mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
+                toolbar.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
 
             }
         });
 
-        imgContainer = findViewById(R.id.img_container);
 
         mParallaxImageHeight = getResources().getDimensionPixelSize(R.dimen.parallax_image_height);
 
-        movieImageView = (ImageView) findViewById(R.id.movie_details_image);
         Glide.with(getApplicationContext()).load(MoviesAdapter.POSTER_BASE_URL+movie.getBackdropPath())
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)
                 .into(movieImageView);
 
 
-        setMovieTitle(toolbar);
+        setMovieTitle();
 
         setToolbarBackgroundColor();
 
-        amvMenu = (ActionMenuView) findViewById(R.id.amvDetailsMenu);
+        setReleaseDate();
 
-        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        setRatingBar(ratingBar);
+        setRatingBar();
 
-        reviewView = (TextView) findViewById(R.id.review);
         setReviewText();
 
 
     }
 
-    private void setRatingBar(RatingBar ratingBar) {
+    private void setReleaseDate() {
+        releaseDateTextView.setText(movie.getReleaseDate());
+    }
+
+    private void setRatingBar() {
         int ratingStarsNumber = 5;
         ratingBar.setRating((Float.parseFloat(movie.getVoteAverage().toString()))/ ratingStarsNumber);
     }
 
-    private void setMovieTitle(Toolbar toolbar) {
-        TextView movieTitle = (TextView) toolbar.findViewById(R.id.details_movie_title);
+    private void setMovieTitle() {
         movieTitle.setText(movie.getTitle());
     }
 
     private Toolbar setToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.movie_detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -107,8 +110,7 @@ public class MovieDetails extends AppCompatActivity{
     }
 
     private void setToolbarBackgroundColor() {
-        mToolbarView = findViewById(R.id.movie_detail_toolbar);
-        mToolbarView.setBackgroundColor(
+        toolbar.setBackgroundColor(
                 ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.colorPrimary)));
     }
 
@@ -117,7 +119,7 @@ public class MovieDetails extends AppCompatActivity{
     }
 
 
-    private Movie getMovieFromIntent() {
+    private Movie getMovieFromIntentExtras() {
         Intent intent = getIntent();
         return intent.getExtras().getParcelable(MoviesAdapter.PARCELABLE_KEY);
     }
